cd1792412787dbcc6a060f3a228075ecf08ad653
Andrew Roldugin <andrew.roldugin@gmail.com>
2018-11-13 01:24:42 +0300

    CalcIC: change min frequency offset init value to doubl max value

--- src/vd/decipher.cc
+++ src/vd/decipher.cc
@@ -98,7 +98,7 @@ double vd::CalcIC(const std::string& text, int offset, int keylength) {
 
 int vd::FindKeyLength(const std::string& text) {
   int out = 0;
-  double min_freqoffset = 0.;
+  double min_freqoffset = std::numeric_limits<double>::max();
   for (int keylength = 1; keylength <= MAX_KEY_LENGTH; ++keylength) {
     double ic = 0.;
     for (int offset = 0; offset < keylength; ++offset) {
@@ -106,7 +106,7 @@ int vd::FindKeyLength(const std::string& text) {
     }
     ic /= keylength;
     double freqoffset = std::abs(IC_ENGLISH - ic);
-    if (!out || freqoffset < min_freqoffset) {
+    if (freqoffset < min_freqoffset) {
       min_freqoffset = freqoffset;
       out = keylength;
     }
