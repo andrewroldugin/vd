96b1624f8fff794260816f0e22c9529181a4e0a0
Andrew Roldugin <andrew.roldugin@gmail.com>
2018-11-13 02:19:21 +0300

    Make letter_freq const

--- src/vd/decipher.cc
+++ src/vd/decipher.cc
@@ -52,7 +52,7 @@ int vd::CountLetters(const std::string& text, int offset, int keylength,
 
 char vd::FindKeyChar(const std::string& text, int offset, int keylength) {
   std::array<double, ALPHABET_SIZE> freq;
-  std::array<double, ALPHABET_SIZE> letter_freq = {
+  const std::array<double, ALPHABET_SIZE> letter_freq = {
     8.167, 1.492, 2.782, 4.253, 12.702, 2.228, 2.015, 6.094, 6.966, 0.153,
     0.772, 4.025, 2.406, 6.749, 7.507, 1.929, 0.095, 5.987, 6.327, 9.056,
     2.758, 0.978, 2.360, 0.150, 1.974, 0.074
