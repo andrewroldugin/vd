9b6aa09260fcb8c0804c859e2a3f9d9a6db1854e
Andrew Roldugin <andrew.roldugin@gmail.com>
2018-11-12 18:58:45 +0300

    wip find key

--- src/vd/decipher.cc
+++ src/vd/decipher.cc
@@ -2,7 +2,9 @@
 
 #include <algorithm>
 #include <array>
+#include <cmath>
 #include <fstream>
+#include <limits>
 #include <numeric>
 #include <sstream>
 #include <stdexcept>
@@ -37,16 +39,39 @@ std::string vd::FindKey(const std::string& text, int keylength) {
 
 char vd::FindKeyChar(const std::string& text, int offset, int keylength) {
   std::array<int, ALPHABET_SIZE> counters;
+  std::array<double, ALPHABET_SIZE> letter_freq = {
+    8.167, 1.492, 2.782, 4.253, 12.702, 2.228, 2.015, 6.094, 6.966, 0.153,
+    0.772, 4.025, 2.406, 6.749, 7.507, 1.929, 0.095, 5.987, 6.327, 9.056,
+    2.758, 0.978, 2.360, 0.150, 1.974, 0.074
+  };
   counters.fill(0);
+  int count = 0;
   for (std::size_t index = offset; index < text.size(); index += keylength) {
     char c = text[index];
     if (std::isalpha(c)) {
+      ++count;
       ++counters[std::tolower(c) - 'a'];
     }
   }
-  auto max_iter = std::max_element(counters.begin(), counters.end());
-  char probably_e = 'a' + std::distance(counters.begin(), max_iter);
-  return probably_e - 'e' + 'A';
+  int caeser_key = 0;
+  double min_chi = std::numeric_limits<double>::max();
+  for (int step = 0; step < ALPHABET_SIZE; ++step) {
+    // calc chi squared
+    // https://en.wikipedia.org/wiki/Chi-squared_test
+    double chi = 0.;
+    for (int index = 0; index < ALPHABET_SIZE; ++index) {
+      int actual = counters[(step + index) % ALPHABET_SIZE];
+      if (actual > 0) {
+        double expected = (letter_freq[index] / 100.) * count;
+        chi += pow(expected - actual, 2) / expected;
+      }
+    }
+    if (chi < min_chi) {
+      min_chi = chi;
+      caeser_key = step;
+    }
+  }
+  return 'A' + caeser_key;
 }
 
 // Calc index of coincidence
